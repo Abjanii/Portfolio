de74496d582f3f2320ed1373e657e31879941c8d
Icappucino <93023359+Abjanii@users.noreply.github.com>
2022-10-20 19:01:41 +0700

    Portfolio

---
+++ dist/js/script.js
@@ -0,0 +1,22 @@
+// navbar-fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+
+// navbar-active
+
+const navbar = document.querySelector('#navbar');
+const navMenu = document.querySelector('#nav-menu');
+
+navbar.addEventListener('click', function(){
+    navbar.classList.toggle('navbar-active');
+    navMenu.classList.toggle('hidden');
+})
